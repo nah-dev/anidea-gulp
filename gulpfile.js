e8587dda79b4548de3f347ac3967f9b6d8fd0401
Mark Whitcher <mark@anidea.co>
2020-03-17 10:33:37 -0500

    ADD comments to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,16 @@ $.reload = $.browserSync.reload;
 
 //################################################################
 
+// This function won't really be used anymore as I'm transitioning to 
+// pug for all my html pre-processing.
 function haml() {
   return gulp.src('source/**/[^_]*.haml')
     .pipe($.haml())
     .pipe(gulp.dest('build'));
 }
 
+
+// Process all sass files for css.
 function sass() {
   return gulp.src('source/**/*.+(sass|scss)')
     .pipe($.sass())
@@ -30,6 +34,9 @@ function sass() {
     .pipe($.sync.stream());
 }
 
+// Process all pug templates
+// This does not include processing through gulp-include because pug
+// has that sort of thing built in.
 function pug() {
   return gulp.src("source/**/[^_]*.pug")
     .pipe($.pug())
@@ -40,6 +47,8 @@ function pug() {
     .pipe(gulp.dest('build'));
 };
 
+// Process all coffeescript files
+// includes processing through gulp-include
 function coffee() {
   return gulp.src("source/**/[^_]*.coffee")
     .pipe($.include())
@@ -47,6 +56,9 @@ function coffee() {
     .pipe(gulp.dest('build'));
 }
 
+// process all javascript files.
+// It runs them through gulp-include and creates uglified versions of 
+// them, including sourcemaps.
 function scripts() {
   return gulp.src("./source/**/[^_]*.js")
     .pipe($.include())
@@ -59,12 +71,21 @@ function scripts() {
     .pipe(gulp.dest('./build'))
 }
 
+// This function processes html files so you can include other filew
+// within them.  This way you can build up a file from multiple parts.
 function html() {
   return gulp.src("./source/**/*.html")
     .pipe($.include())
     .pipe(gulp.dest("./build"))
 }
 
+// This is used for building Net-at-hand plugins.  It simply processes
+// ruby files for the gulp-include syntax so you can build the plugin
+// code without relying on the order of the files in the filesystem.
+// 
+// For plugins, there needs to be an output.rb file within the source
+// folder that then gets processed into the build folder and picked
+// up by the rake tasks for the rest of it.
 function ruby() {
   return gulp.src("./source/**/*.rb")
     .pipe($.include({
@@ -76,6 +97,9 @@ function ruby() {
     .pipe(gulp.dest("./build"))
 }
 
+
+// I don't know that I really use this one at all, but for compatibility
+// I am leaving it in.
 gulp.task('compile', gulp.parallel(
   sass,
   pug,
@@ -85,6 +109,10 @@ gulp.task('compile', gulp.parallel(
 
 //################################################################
 
+// I haven't really fleshed this one out completely, but the idea
+// is that certain files were meant to be uploaded to a counterpart
+// on net-at-hand.  This would let me build a page locally but upload
+// it to the website.
 function nah() {
   return gulp.src("./build/**/*.nah.html")
     .pipe($.nah.updatePage())
