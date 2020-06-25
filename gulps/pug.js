db9752a03e86aa97a3e4aa4c0edd0851fe05709c
Mark Whitcher <mark@anidea.co>
2020-06-25 10:08:49 -0500

    ADD touch to pug.js so partials will reload browser

--- gulps/pug.js
+++ gulps/pug.js
@@ -1,6 +1,7 @@
 const gulp  = require('gulp');
 const $     = require("./setup");
 
+
 // Process all pug templates
 // This does not include processing through gulp-include because pug
 // has that sort of thing built in.
@@ -12,5 +13,6 @@ module.exports = function pug() {
       this.emit('end');
     })
     .pipe($.revReplace({manifest: gulp.src("build/manifest.json", {allowEmpty: true})}))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
+    .pipe($.touchFd());
 };
